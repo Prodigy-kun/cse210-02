26a08d12b57672116007abcc3a7b71e724e84b80
xkevd <kevinalex11@hotmail.com>
2022-05-25 22:46:33 -0300

    Parachute class

---
+++ jumper game/parachute.py
@@ -0,0 +1,29 @@
+class parachute:
+    '''
+    Initial drawing: 
+      ___
+     /___\\
+    \\   /
+     \\ /
+
+    '''
+    def __init__(self) -> None:
+        self._draw = [" ___","/___\\","\\   /"," \\ /"]
+
+    '''
+     Draw parachute
+    '''
+    def draw_parachute(self):
+        for i in self._draw:
+            print(i)
+
+    '''
+     Delete one segment
+    '''
+    def delete_segment(self):
+        self._draw.pop(1)
+
+        '''True if there is no more parachute'''
+    def game_over(self):
+        return self._draw == []
+
