0b5c6b0ea402ec7d91a487bc2a9bf933de7ccad3
wazuu <smi21105@byui.edu>
2022-05-26 16:27:17 -0700

    code for the alphabet

---
+++ jumper game/alphabet.py
@@ -0,0 +1,52 @@
+# Jumper Game Outline 
+
+"""def main()
+create all game objects
+"""
+
+"""class parachute()
+create the parachute,
+delete segments,
+Kevin
+"""
+
+"""class alphabet()
+word list using all 26 letters,
+add letters to lines if correct,
+Tim
+"""
+alphabet = []
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+class alphabet:
+    def alphabet():
+        if alphabet == alphabet: 
+            print(keep line)
+
+        elif alphabet != alphabet:
+            print(remove line)
+
+        else:
+            print("that is not a valid entry")
+
+
+
+
+
+"""class draw person
+create the person,
+alive or dead,
+Michael
+"""
+
+"""class game
+pair the guess with the word/letter,
+method = game over, guess it correctly or parachute is gone,
+Robert
+"""
+
+""" create a README file
+introduction,
+rules of the game,
+software that is needed,
+creators including names, and emails
+"""
