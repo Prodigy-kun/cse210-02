3d415f7aafe88951811b3eae5e6d79e0ae65564c
prodigy_kun <ilerioluwaomotayo@gmail.com>
2022-05-27 01:41:50 +0100

    update

---
+++ jumper_game/game/draw_person.py
@@ -0,0 +1,25 @@
+# A class(draw_person) that draws the jumper dead and alive
+class Draw_person:
+    #attributes(none)
+    # methods:person_alive()
+    #          person_dead
+    def person_alive(self):
+        """Args:
+            self (Die): An instance of Die.
+        """
+        #draw the person while alive
+        print('   0   ')
+        print('  /|\\')
+        print('  / \\')
+        print('^^^^^^^')
+    def persom_dead(self):
+        """Args:
+            self (Die): An instance of Die.
+        """
+         #draw the person while dead
+        print('   x   ')
+        print('  /|\\')
+        print('  / \\')
+        print('^^^^^^^')
+
+
