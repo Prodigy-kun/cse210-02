d1dc5ffa2469b200a826a14af239f9ced8ebdaac
Robert Elliott <robert.elliott2925@gmail.com>
2022-05-26 19:00:02 -0600

    word_list class

--- jumper game/word_list.py
+++ jumper game/word_list.py
@@ -1,6 +1,8 @@
 import random
 
 class Word_list:
+    """_summary_
+    """
     def __init__(self):
         self.word_list = ["blank", "chess", "games", "shoot", "stomp", "books", "erase", "raise", "great", "keeps"]
         
@@ -8,6 +10,3 @@ class Word_list:
         self.word = self.word_list[random.randint(0,9)]
         return self.word
     
-list = Word_list()
-word = list.get_word()
-print(word)
