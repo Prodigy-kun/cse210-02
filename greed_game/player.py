ea1fb7544ccba5790514a1fc83cc96d1f7fe6727
wazuu <smi21105@byui.edu>
2022-06-09 15:44:39 -0700

    players and rocks code

---
+++ greed_game/player.py
@@ -0,0 +1,33 @@
+class Player:
+    def move_player(self, max_x, max_y):
+        """Moves the player to its next position using the x and y values
+        
+        Args:
+            max_x (int): The maximum x value.
+            max_y (int): The maximum y value.
+        """
+        x = (self._position.get_x() + self._velocity.get_x()) % max_x
+        y = (self._position.get_y() + self._velocity.get_y()) % max_y
+        self._position = Point(x, y)
+    
+    def get_position(self):
+        """Gets the players position in 2d space.
+        
+        Returns:
+            Point: The actor's position in 2d space.
+        """
+        return self._position
+    
+    def get_text(self):
+        """Gets the players textual representation."""
+        return self._text
+
+    def get_velocity(self):
+        """Gets the actor's speed and direction.
+        
+        Returns:
+            Point: The actor's speed and direction.
+        """
+        return self._velocity
+    
+    
