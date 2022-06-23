9adbb5aa9ee844653c39e84143f0450dcbfaa2e0
xkevd <kevinalex11@hotmail.com>
2022-06-23 18:29:57 -0300

    Kevin

--- cycle/game_over.py
+++ cycle/game_over.py
@@ -4,10 +4,10 @@ class game_over():
 
     def __init__(self):
         self._color = Color(255,255,255)
-        self._text = ""
-
-    def set_text(self):
-        self._text ="Game over"
+        self._text = "Game over"
 
     def get_text(self):
         return self._text
+
+    def get_color(self):
+        return self._color
