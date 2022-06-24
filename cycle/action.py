2fe8a84e972e0423557c970f8ff92c803ca883fd
Robert Elliott <robert.elliott2925@gmail.com>
2022-06-23 18:35:31 -0600

    action class

---
+++ cycle/action.py
@@ -0,0 +1,13 @@
+class Action:
+    """An action that is done.
+    
+    The responsibility of action is to do somthing within in the game.
+    It has one method, execute(), which should be overridden by derived classes.
+    """
+    def execute(self):
+        """Executes something that is important in the game. Method to be overidden.
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        pass
